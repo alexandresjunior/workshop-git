8854ae7ba1b2da8a746449116dc1d2d21b093a0b
Rodrigo Batista <rodrigobatista03@gmail.com>
2025-02-08 15:23:17 -0300

    Solicitação de peso e altura do usuário

--- backend/src/Rodrigo.java
+++ backend/src/Rodrigo.java
@@ -1,9 +1,17 @@
-
+import java.util.Scanner;
 
 public class Rodrigo {
 
     public static void main(String[] args) throws Exception {
         System.out.println("Olá Mundo!");
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Qual é o seu peso:");
+        double peso = entrada.nextDouble();
+
+        System.out.println("Qual é sua altura: ");
+        double altura = entrada.nextDouble();
         
 
     }
