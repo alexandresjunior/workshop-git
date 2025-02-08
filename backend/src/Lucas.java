fb2f3e4b888378d5aaf69c7a92adff81de982b27
Lucas Vorique <vorique.ads@gmail.com>
2025-02-08 15:19:55 -0300

    Criação da solicitação de peso e altura do paciente.

---
+++ backend/src/Lucas.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Lucas {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Olá mundo Java!");
+
+        System.out.println("Insira o peso do paciente: ");
+        double pesoPaciente = sc.nextDouble();
+
+        System.out.println("Insira a altura do paciente: ");
+        double alturaPaciente = sc.nextDouble();
+
+        
+        sc.close();
+    }
+}
