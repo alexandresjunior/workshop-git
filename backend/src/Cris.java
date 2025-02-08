e7ea24cb372835d561b42bd0ec6b69e30d150886
Janiedja <janiedja@hotmail.com>
2025-02-08 15:45:41 -0300

    Exercicio2

--- backend/src/Cris.java
+++ backend/src/Cris.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Cris {public static void main(String[] args) {
-    System.out.println("Olá, mundo!");
-}
     
+Scanner entrada = new Scanner (System.in);
+
+System.out.println("Digite o seu peso:   ");
+Double peso = entrada.nextDouble();
+
+System.out.println("Digite sua altura:  ");
+Double altura = entrada.nextDouble();
+
+}
 }
