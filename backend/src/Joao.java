ee807a9f35c433377e85d31dd8d827dda3a54277
João Lázaro <ijoaolazaro7@gmail.com>
2025-02-08 15:59:37 -0300

    Solicitação de peso e altura

---
+++ backend/src/Joao.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Joao {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        //PERGUNTAS(SCANNERS)
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite seu peso corporal");
+        float peso = sc.nextFloat();
+        System.out.println("Qual sua altura: ");
+        float altura = sc.nextFloat();
+        //PROCESSAMENTO(CALCULO)
+        
+    }
+}
