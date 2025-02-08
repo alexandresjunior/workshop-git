c2d4384dd77ee558e5b0d9dfe244023e7deddf42
Janiedja <janiedja@hotmail.com>
2025-02-08 15:48:28 -0300

    Exercicio3

--- backend/src/Cris.java
+++ backend/src/Cris.java
@@ -10,5 +10,8 @@ Double peso = entrada.nextDouble();
 System.out.println("Digite sua altura:  ");
 Double altura = entrada.nextDouble();
 
+Double imc = peso / (altura * altura);
+System.out.println("Com altura " + altura + "e peso" + "  , o IMC fica:  " + imc);
+
 }
 }
