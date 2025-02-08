67d0fe27ffdf2724c894831d1d0a82a00e3db636
Luiz Matheus <nomenenhum190@gmail.com>
2025-02-08 15:25:33 -0300

    IMC

--- backend/src/Cris.java
+++ backend/src/Cris.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Cris {
     public static void main(String[] args) {
         System.out.println("Olá, mundo");
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite seu peso: ");
+
+        Double peso = entrada.nextDouble();
+
+        System.out.println("Digite sua altura: ");
+
+        Double altura = entrada.nextDouble();
+
+        Double imc = peso / (altura * altura); 
+
+        System.out.println("Com altura " + altura + " e peso " + peso + ", o IMC fica " + imc);
     }
 }
